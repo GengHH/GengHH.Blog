61a316e2a821ef6844727ee1324fc73608d4acf7
GengHH <18818060415@163.com>
2023-05-10 17:42:33 +0800

    添加博客文件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,96 +8,101 @@
  */
 module.exports = {
 	title: "Genghh's Blog",
-	description: "use vuepress@1.x and vuepress-theme-reco ",
-	dest: "public",
+	description: 'use vuepress@1.x and vuepress-theme-reco ',
+	dest: 'public',
 	head: [
 		[
-			"link",
+			'link',
 			{
-				rel: "icon",
-				href: "/favicon.ico",
+				rel: 'icon',
+				href: '/favicon.ico',
 			},
 		],
 		[
-			"meta",
+			'meta',
 			{
-				name: "viewport",
-				content: "width=device-width,initial-scale=1,user-scalable=no",
+				name: 'viewport',
+				content: 'width=device-width,initial-scale=1,user-scalable=no',
 			},
 		],
 	],
-	theme: "reco",
+	theme: 'reco',
 	themeConfig: {
 		nav: [
 			{
-				text: "Home",
-				link: "/",
-				icon: "reco-home",
+				text: 'Home',
+				link: '/',
+				icon: 'reco-home',
 			},
 			{
-				text: "TimeLine",
-				link: "/timeline/",
-				icon: "reco-date",
+				text: 'TimeLine',
+				link: '/timeline/',
+				icon: 'reco-date',
 			},
 			{
-				text: "Docs",
-				icon: "reco-message",
+				text: 'Docs',
+				icon: 'reco-message',
 				items: [
 					{
-						text: "vuepress-reco",
-						link: "/docs/theme-reco/",
+						text: 'vuepress-reco',
+						link: '/docs/theme-reco/',
+					},
+					{
+						text: 'learn',
+						link: '/docs/learn/',
 					},
 				],
 			},
 			{
-				text: "Contact",
-				icon: "reco-message",
+				text: 'Contact',
+				icon: 'reco-message',
 				items: [
 					{
-						text: "GitHub",
-						link: "https://github.com/GengHH",
-						icon: "reco-github",
+						text: 'GitHub',
+						link: 'https://github.com/GengHH',
+						icon: 'reco-github',
 					},
 				],
 			},
 		],
 		sidebar: {
-			"/docs/theme-reco/": ["", "theme", "plugin", "api"],
+			'/docs/theme-reco/': ['', 'theme', 'plugin', 'api'],
+			'/docs/learn/': ['', 'vue'],
 		},
-		type: "blog",
+		type: 'blog',
 		blogConfig: {
 			category: {
 				location: 2,
-				text: "Category",
+				text: 'Category',
 			},
 			tag: {
 				location: 3,
-				text: "Tag",
+				text: 'Tag',
 			},
 		},
 		friendLink: [
 			{
-				title: "午后南杂",
-				desc: "Enjoy when you can, and endure when you must.",
-				email: "1156743527@qq.com",
-				link: "https://www.recoluan.com",
+				title: '午后南杂',
+				desc: 'Enjoy when you can, and endure when you must.',
+				email: '1156743527@qq.com',
+				link: 'https://www.recoluan.com',
 			},
 			{
-				title: "vuepress-theme-reco",
-				desc: "A simple and beautiful vuepress Blog & Doc theme.",
+				title: 'vuepress-theme-reco',
+				desc: 'A simple and beautiful vuepress Blog & Doc theme.',
 				avatar:
-					"https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-				link: "https://vuepress-theme-reco.recoluan.com",
+					'https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png',
+				link: 'https://vuepress-theme-reco.recoluan.com',
 			},
 		],
-		logo: "/logo.png",
+		logo: '/logo.png',
 		search: true,
 		searchMaxSuggestions: 10,
-		lastUpdated: "Last Updated",
-		author: "GengHH",
-		authorAvatar: "/avatar.png",
-		record: "xxxx",
-		startYear: "2017",
+		lastUpdated: 'Last Updated',
+		author: 'GengHH',
+		authorAvatar: '/avatar.png',
+		record: 'xxxx',
+		startYear: '2017',
 	},
 	markdown: {
 		lineNumbers: true,
